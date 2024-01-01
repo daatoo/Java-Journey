53969535854c7a490c4aaa379877c80d32f81ab3
david <davit.cheishvili.06@gmail.com>
2024-01-01 22:46:18 +0400

    add Streams, Stack and List

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-public class Main {
-    public static  void main(String[] args) {
-        Stream<Integer> stream = Stream.of(25, 15, 75, 35, 40, 5, 10, 55, 60);
-        stream.parallel().forEach(System.out::println);
-    }
-}
