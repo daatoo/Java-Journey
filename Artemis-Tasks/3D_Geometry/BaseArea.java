f0b96f6653d49e094b394c30aad5a0c55386f902
david <davit.cheishvili.06@gmail.com>
2024-01-02 13:01:04 +0400

    add 3D geometry solution

---
+++ Artemis-Tasks/3D_Geometry/BaseArea.java
@@ -0,0 +1,8 @@
+package fop.w7geo;
+
+public abstract class BaseArea {
+   abstract double circumference();
+   abstract double area();
+   abstract boolean isSquare();
+   abstract Square toSquare();
+}
