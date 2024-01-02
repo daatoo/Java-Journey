73612c364a810b5c5623344c097bff823a9c3935
david <davit.cheishvili.06@gmail.com>
2024-01-02 21:56:52 +0400

    add FileTree solution

---
+++ Artemis-Tasks/FileTree/RegularFile.java
@@ -0,0 +1,42 @@
+package filetree;
+
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RegularFile extends File {
+
+	public RegularFile(Path path) {
+		super(path);
+	}
+
+	@Override
+	public Iterator<File> iterator() {
+		return new Iterator<File>() {
+			boolean visited = false;
+			@Override
+			public File next() {
+				if (visited) {
+					throw new NoSuchElementException();
+				}
+				visited = true;
+				return RegularFile.this;
+			}
+			@Override
+			public boolean hasNext() {
+				return !visited;
+			}
+		};
+	}
+
+	@Override
+	public int getHeight() {
+		return 0;
+	}
+
+	@Override
+	public boolean isRegularFile() {
+		return true;
+	}
+
+}
